ab71e5ebf7d18e03d8547f6cac198d6af99b1713
M1shaG <mgnedyonok@GMAIL.com>
2018-07-25 16:02:41 +0300

    Finish demo app

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+applic.application.routes.draw do
+  resources :users#index to: 'controller#action'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
